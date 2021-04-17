f80e8a7c931f71f810bf7518bfb5b9b7a164af19
unknown <amolskarpe@gmail.com>
2021-04-17 22:22:21 +0530

    first commit

--- app1/models.py
+++ app1/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Ques(models.Model):
 	symbol = models.CharField(max_length=200, null=True, blank=True)
-	date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	date = models.DateField(null=True, blank=True)
 	open1 = models.DecimalField(max_digits=10, decimal_places=4, null=True, blank=True)
 	high = models.DecimalField(max_digits=10, decimal_places=4, null=True, blank=True)
 	low = models.DecimalField(max_digits=10, decimal_places=4, null=True, blank=True)
 	close = models.DecimalField(max_digits=10, decimal_places=4, null=True, blank=True)
-	vol = models.DecimalField(max_digits=10, decimal_places=4, null=True, blank=True)
+	vol = models.IntegerField(null=True, blank=True)
 	adj = models.DecimalField(max_digits=10, decimal_places=4, null=True, blank=True)
 
 	
